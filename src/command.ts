a696bb88a80b680dfb53fc6c9e9ca9294de7e991
Yusuke Yagyu (gyugyu) <gyugyu@pepabo.com>
2020-10-14 15:48:56 +0900

    fix lack of file argument assignment

--- src/command.ts
+++ src/command.ts
@@ -7,24 +7,26 @@ function separateByColon(value: string): string[] {
   return value.split(':')
 }
 
+interface Command {
+  includePaths?: string[]
+}
+
 program
   .version(pkgJson.version)
   .arguments('[file]')
   .option('-I --include-paths <path>', 'include paths', separateByColon)
+  .action((file: string | undefined, cmd: Command) => {
+    file = file ?? '**/*.test.scss'
 
+    const reporter = sassunit(file, {
+      includePaths: cmd.includePaths
+    })
+
+    reporter.report()
+
+    if (reporter.hasError()) {
+      process.exit(1)
+    }
+  })
 
 program.parse(process.argv)
-
-const options = {
-  includePaths: program.includePaths as undefined | string[]
-}
-
-const file = program.file ?? '**/*.test.scss'
-
-const reporter = sassunit(file, options)
-
-reporter.report()
-
-if (reporter.hasError()) {
-  process.exit(1)
-}
